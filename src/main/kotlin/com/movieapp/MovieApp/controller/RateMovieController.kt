5257f2ffe3bf3dc10d73995fa5d5ebc85f785441
Marco Lehne <lehne.mar@gmail.com>
2023-07-10 18:06:38 +0200

    feat: added RateMovieController with integration test

--- src/main/kotlin/com/movieapp/MovieApp/controller/RateMovieController.kt
+++ src/main/kotlin/com/movieapp/MovieApp/controller/RateMovieController.kt
@@ -1,19 +1,22 @@
-//package com.movieapp.MovieApp.controller
-//
-//import com.movieapp.MovieApp.model.Movie
-//import com.movieapp.MovieApp.model.MovieRatingRequest
-//import com.movieapp.MovieApp.service.MovieService
-//import org.springframework.web.bind.annotation.PatchMapping
-//import org.springframework.web.bind.annotation.RequestBody
-//import org.springframework.web.bind.annotation.RequestMapping
-//import org.springframework.web.bind.annotation.RestController
-//
-//@RestController
-//@RequestMapping("/rate")
-//class RateMovieController(private val service: MovieService) {
-//
-//    @PatchMapping
-//    fun rateMovie(@RequestBody movieRatingRequest: MovieRatingRequest): Movie {
-//        return service.rateMovie(movieRatingRequest)
-//    }
-//}
+package com.movieapp.MovieApp.controller
+
+import com.movieapp.MovieApp.model.Movie
+import com.movieapp.MovieApp.model.MovieRatingRequest
+import com.movieapp.MovieApp.service.UserActionService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/rateMovie")
+class RateMovieController(
+    private val service: UserActionService
+) {
+
+    @PostMapping
+    fun rateMovie(@RequestBody userRatingRequest: MovieRatingRequest): Movie {
+        return service.rateMovie(userRatingRequest)
+    }
+
+}
